cbf9c35c7623cb754a548c46c948cdb2c1c961f9
Aleksey Z <valjean.dev@gmail.com>
2026-05-16 21:30:11 +0300

    Add audit function

--- storage_doctor.sh
+++ storage_doctor.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+
+#-----------------------------
+# Error exception functions
+
 error_no_args()
 {
     echo "$0: no arguments for the script" # name script file
@@ -18,6 +22,14 @@ error_unrec_args()
     echo "Try '$0 --help' for more information."  
 }
 
+error_unrec_numb_function()
+{
+    echo "$0: an unrecognized numb function $func_var" 
+}
+
+#-----------------------------
+# Help function
+
 help_info()
 {
     echo "STORAGE DOCTOR."
@@ -32,6 +44,64 @@ help_info()
     echo
 }
 
+#-----------------------------
+# Audit functions
+
+show_disk_info()
+{
+    echo "show_disk_info"
+    #df -h
+    #df -i
+    #lsblk
+    #findmnt
+}
+
+show_top_10_hard_dir()
+{
+    echo "show_top_10_hard_dir"
+}
+
+search_big_files()
+{
+    echo "search_big_files"
+}
+
+search_del_open_files()
+{
+    echo "search_del_open_files"
+}
+
+audit()
+{
+   echo "Audit function: "
+   echo "1 - Show disk info (df -h, df -i, lsblk, findmnt)"
+   echo "2 - Show top 10 big dir"
+   echo "3 - Search big files"
+   echo "4 - Search delete-open files"
+   echo -n "Select number of function: " # input value
+   read func_var # get value
+   case $func_var in
+            1)
+                show_disk_info
+                ;;
+            2)
+                show_top_10_hard_dir
+                ;;
+            3)
+                search_big_files
+                ;;
+            4)
+                search_del_open_files
+                ;;
+            *)
+                error_unrec_numb_function "$func_var"
+                ;;
+        esac
+}
+
+#-----------------------------
+# Main block
+
 if [[ $# -eq 0 ]]; then # check count args
     error_no_args
 else
@@ -43,7 +113,7 @@ else
                 help_info
                 ;;
             --audit)
-                echo "audit info"
+                audit
                 ;;
             --fix)
                 echo "fix info"
